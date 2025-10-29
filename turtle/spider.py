557bef5b39e6538d624679ed68b235b2aab23db3
Anastasiya F. <polkova87@mail.ru>
2025-10-29 13:58:52 +0300

    add spider

---
+++ turtle/spider.py
@@ -0,0 +1,11 @@
+from turtle import *
+import time
+
+shape('turtle')
+#https://judge.mipt.ru/mipt_cs_on_python3/labs/lab1.html#id41
+
+for _ in range(12):
+    left(30)
+    forward(100)
+    stamp()
+    backward(100)
